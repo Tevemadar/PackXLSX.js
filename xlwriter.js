5ae684f6c6883ef2ab9d31540f0f6489d8dc6711
Gergely Csucs <Tevemadar@users.noreply.github.com>
2023-05-09 17:23:05 +0200

    Comments, strings

--- xlwriter.js
+++ xlwriter.js
@@ -1,5 +1,5 @@
-const xml_head = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>`;
-const ct_fn = `[Content_Types].xml`;
+const xml_head = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
+const ct_fn = '[Content_Types].xml';
 const ct_dt = `${xml_head}
 <Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types">
 <Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/>
@@ -8,73 +8,34 @@ const ct_dt = `${xml_head}
 <Override PartName="/xl/worksheets/sheet1.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml"/>
 <Override PartName="/xl/sharedStrings.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sharedStrings+xml"/>
 </Types>`;
-const rels_fn = `_rels/.rels`;
+const rels_fn = '_rels/.rels';
 const rels_dt = `${xml_head}
 <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
 <Relationship Id="r" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument" Target="xl/workbook.xml"/>
 </Relationships>`;
-const xlrels_fn = `xl/_rels/workbook.xml.rels`;
+const xlrels_fn = 'xl/_rels/workbook.xml.rels';
 const xlrels_dt = `${xml_head}
 <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
 <Relationship Id="r1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet" Target="worksheets/sheet1.xml"/>
 <Relationship Id="r2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/sharedStrings" Target="sharedStrings.xml"/>
 </Relationships>`;
-const wb_fn = `xl/workbook.xml`;
+const wb_fn = 'xl/workbook.xml';
 const wb_dt = `${xml_head}
 <workbook xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" mc:Ignorable="x15" xmlns:x15="http://schemas.microsoft.com/office/spreadsheetml/2010/11/main">
 <sheets>
 <sheet name="Sheet1" sheetId="1" r:id="r1"/>
 </sheets>
 </workbook>`;
-const sh_fn = `xl/worksheets/sheet1.xml`;
-//const sh_dt=`${xml_head}
-//<worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
-//<sheetData>
-//<row>
-//<c r="A1" t="s"><v>0</v></c>
-//<c r="C1" t="s"><v>1</v></c>
-//</row>
-//<row/>
-//<row>
-//<c r="A3"><v>1.1000000000000001</v></c>
-//<c r="B3"><v>2.2000000000000002</v></c>
-//<c r="C3"><f>A3*B3</f></c>
-//</row>
-//</sheetData>
-//</worksheet>`;
+const sh_fn = 'xl/worksheets/sheet1.xml';
 const sh_head = `${xml_head}
 <worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
 <sheetData>`;
 const sh_tail = `</sheetData>
 </worksheet>`;
-const st_fn = `xl/sharedStrings.xml`;
-//const st_dt=`${xml_head}
-//<sst xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
-//<si><t>Hello</t></si>
-//<si><t>eh</t></si>
-//</sst>`;
+const st_fn = 'xl/sharedStrings.xml';
 const st_head = `${xml_head}
 <sst xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">`;
 const st_tail = `</sst>`;
-
-//function doThing() {
-//    const te = new TextEncoder();
-//    const date = new Date;
-//    const blob = zipstore([
-//        {name:ct_fn, date, data:te.encode(ct_dt)},
-//        {name:rels_fn, date, data:te.encode(rels_dt)},
-//        {name:xlrels_fn, date, data:te.encode(xlrels_dt)},
-//        {name:wb_fn, date, data:te.encode(wb_dt)},
-//        {name:sh_fn, date, data:te.encode(sh_dt)},
-//        {name:st_fn, date, data:te.encode(st_dt)}
-//    ]);
-//    const url = URL.createObjectURL(blob);
-//    const a = document.createElement("a");
-//    a.href = url;
-//    a.download = "test.xlsx";
-//    a.click();
-//    URL.revokeObjectURL(url);
-//}
 
 function doXl(sheet) {
     const string_table = [];
